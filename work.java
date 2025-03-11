7bc88a343d4213b8a674a7ddba0877e23070b257
VOOTLA SAI CHANDU <saichanduvootla@gmail.com>
2025-03-12 00:50:55 +0530

    Multi threading programs

---
+++ work.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+//@SuppressWarnings("unused")
+public class work {
+
+	public static void main(String args[]) {
+	work2 oj = new work2();
+	oj.start();
+	for (int i=1; i <=5;i++) {
+		System.out.println("from main roi"+ i);
+	}
+	}
+}
