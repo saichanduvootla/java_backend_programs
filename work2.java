6ab22be524a6e29f2ad11dcd27f4d41475a72d99
VOOTLA SAI CHANDU <saichanduvootla@gmail.com>
2025-03-12 23:45:08 +0530

    Update work2.java

--- work2.java
+++ work2.java
@@ -1,5 +1,7 @@
 
-public class work2 extends Thread{
+
+public class work2 implements Runnable{
+	
 public void run() {
 	for (int i =1; i<=5;i++) {
 		System.out.println(Thread.currentThread().getName()+"i = "+ i);
