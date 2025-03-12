3958814d705653b67d53c66e26e0ec4fec17ccbd
VOOTLA SAI CHANDU <saichanduvootla@gmail.com>
2025-03-12 23:45:39 +0530

    Add files via upload

---
+++ work3.java
@@ -0,0 +1,11 @@
+
+public class work3 extends Thread{
+	 
+		 public void run() {
+		 	for (int i =1; i<=5;i++) {
+		 		System.out.println(Thread.currentThread().getName()+"i = "+ i);
+		 	}
+		 }
+		 	
+		 }
+
