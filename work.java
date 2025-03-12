0474d45d6984b0f80d1e2fb757b05aad26552370
VOOTLA SAI CHANDU <saichanduvootla@gmail.com>
2025-03-12 23:44:01 +0530

    Update work.java

--- work.java
+++ work.java
@@ -4,6 +4,10 @@ import java.io.*;
 public class work {
 
 	public static void main(String args[]) {
+
+	work3 oj2 = new  work3();
+	oj2.start();
+		
 	work2 oj = new work2();
 	oj.start();
 	for (int i=1; i <=5;i++) {
